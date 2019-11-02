41ceff9afae7792bc1991e7a3a2f379cf3e4318f
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-02 13:22:58 -0400

    initial commit

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smooth_vec;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this gives us access to the player game object (the capsule)
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        movement = Vector2.Scale(movement, new Vector2(smoothing * sensitivity, smoothing * sensitivity));
+        smooth_vec.x = Mathf.Lerp(smooth_vec.x, movement.x, 1.0f / smoothing);
+        smooth_vec.y = Mathf.Lerp(smooth_vec.y, movement.y, 1.0f / smoothing);
+        mouseLook += smooth_vec;
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float move_speed = 5.0f; //the movement speed of the character
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //turn off the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //forward backward translation
+        float front_back = Input.GetAxis("Vertical") * Time.deltaTime * move_speed;
+        //left right translation
+        float side = Input.GetAxis("Horizontal") * Time.deltaTime * move_speed;
+
+        transform.Translate(side, -front_back * Mathf.Sign(this.GetComponentInChildren<Camera>().gameObject.transform.localRotation.x), front_back);
+    }
+}
