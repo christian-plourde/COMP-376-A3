eb140676ac12773dc3932e6d54462a4811a10255
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-11-03 11:20:26 -0500

    added score functionality

---
+++ Assets/Scripts/BoatInventory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoatInventory : MonoBehaviour
+{
+    //this script manages the boat's inventory, which is essentially the player's score
+    int gold_count;
+    PlayerGoldInventory player_inventory;
+    public Text score_text;
+
+    public int Score
+    {
+        get { return gold_count; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gold_count = 0;
+        player_inventory = FindObjectOfType<PlayerGoldInventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        //if the colliding object is the player dump his gold into the boat
+        if(col.CompareTag("Player"))
+        {
+            gold_count += player_inventory.TotalGold;
+            player_inventory.EmptyGold();
+
+            //finally update the score text
+            score_text.text = "Score: " + Score;
+        }
+    }
+}
--- Assets/Scripts/PlayerGoldInventory.cs
+++ Assets/Scripts/PlayerGoldInventory.cs
@@ -23,6 +23,21 @@ public class PlayerGoldInventory : MonoBehaviour
         }
     }
 
+    public void EmptyGold()
+    {
+        total_gold = 0;
+        player_controller.SpeedMultiplier = 1.0f - 0.9f * (((float)total_gold / (float)capacity));
+        //we also need to update the player inventory bar ui element
+        float amount_filled = (float)total_gold / (float)capacity;
+        inventory_bar.rectTransform.offsetMin = new Vector2(inventory_bar.rectTransform.offsetMin.x, 100.0f - 100.0f * amount_filled);
+    }
+
+    public int TotalGold
+    {
+        get { return total_gold; }
+        set { total_gold = value; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +49,8 @@ public class PlayerGoldInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //panic mode (if the player presses q, he will drop all his gold. In the event of an emergency)
+        if (Input.GetKeyDown(KeyCode.Q))
+            EmptyGold();
     }
 }
